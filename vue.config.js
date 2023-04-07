c93d08a0b5abb142bfde176e9aa91366d3ad20e8
owocc <owocc@qq.com>
2023-04-07 14:33:25 +0800

    project init

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    productionSourceMap: false,
+    publicPath: '/VUE',
+    outputDir: 'dist',
+    assetsDir: 'static',
+    devServer: {
+        host: '0.0.0.0',
+        port: 8082,
+        proxy: {
+            '/API': {
+                target: 'http://localhost:8081',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/API': '/API'
+                }
+            }
+        }
+    },
+}
